eac8390a3274dfaf536591f801e3c5ffa58587f7
Robert Lacey <rob@rplaceyltd.co.uk>
2020-02-10 11:16:09 +0000

    Inital commit

---
+++ Assets/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    enum Difficultly
+    {
+        Easy, Medium, Hard
+    }
+
+    [SerializeField] private Difficultly myDifficulty;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (myDifficulty)
+            {
+                case Difficultly.Easy:
+                    Debug.Log("I'm in easy");
+                    break;
+                case Difficultly.Medium:
+                    Debug.Log("I'm in Medium");
+                    break;
+                case Difficultly.Hard:
+                    Debug.Log("I'm in Hard");
+                    break;
+                default:
+                    Debug.Log("Invalid");
+                    break;
+            }
+        }
+    }
+}
